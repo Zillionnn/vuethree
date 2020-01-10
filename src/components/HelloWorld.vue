05a11bffcf428486fbdcf19dd51a3d4c997dc7be
Administrator <skyestzhang@163.com>
2020-01-10 09:53:09 +0800

    init

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <el-date-picker v-model="value1" type="date" placeholder="选择日期">
+    </el-date-picker>
+    <el-button type="text" @click="open">点击打开 Message Box</el-button>
+    <el-button plain @click="open1">
+      成功xx
+    </el-button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      value1: '',
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  methods: {
+    open () {
+      this.$alert('这是一段内容', '标题名称', {
+        confirmButtonText: '确定',
+        callback: action => {
+          this.$message({
+            type: 'info',
+            message: `action: ${action}`
+          })
+        }
+      })
+    },
+
+    open1 () {
+      this.$notify({
+        title: '成功',
+        message: '这是一条成功的提示消息',
+        type: 'success'
+      })
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1,
+h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
