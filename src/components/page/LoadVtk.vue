6e4ee0610102cf43626f42ab29ce226d69c555d3
Administrator <skyestzhang@163.com>
2020-01-10 10:59:18 +0800

    load vtk

--- src/components/page/LoadVtk.vue
+++ src/components/page/LoadVtk.vue
@@ -1,14 +1,17 @@
 <template>
  <div class="page">
-
+   <div id="3d"></div>
  </div>
 </template>
 
 <script>
 import * as THREE from 'three'
 import Stats from '@/jsm/libs/stats.module.js'
-import { OrbitControls } from '@/jsm/controls/OrbitControls.js'
-import { TGALoader } from '@/jsm/loaders/TGALoader.js'
+// import { OrbitControls } from '@/jsm/controls/OrbitControls.js'
+// import { TGALoader } from '@/jsm/loaders/TGALoader.js'
+
+import { TrackballControls } from '@/jsm/controls/TrackballControls.js'
+import { VTKLoader } from '@/jsm/loaders/VTKLoader.js'
 
 export default {
   name: '',
@@ -38,68 +41,105 @@ export default {
       let camera = null
       let scene = null
       let geometry = null
-      let material = null
+      // let material = null
       let mesh = null
       let renderer = null
+      let controls = null
+      let stats = null
 
-      var container = document.createElement('div')
-      document.body.appendChild(container)
-
-      camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 2000)
-      camera.position.set(0, 50, 250)
+      camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 0.01, 1e10)
+      camera.position.z = 0.2
 
       scene = new THREE.Scene()
 
-      //
+      scene.add(camera)
 
-      var loader = new TGALoader()
-      geometry = new THREE.BoxBufferGeometry(50, 50, 50)
+      // light
 
-      // add box 1 - grey8 texture
-      var texture1 = loader.load('textures/crate_grey8.tga')
-      var material1 = new THREE.MeshPhongMaterial({ color: 0xffffff, map: texture1 })
+      var dirLight = new THREE.DirectionalLight(0xffffff)
+      dirLight.position.set(200, 200, 1000).normalize()
 
-      var mesh1 = new THREE.Mesh(geometry, material1)
-      mesh1.position.x = -50
+      camera.add(dirLight)
+      camera.add(dirLight.target)
 
-      scene.add(mesh1)
+      var material = new THREE.MeshLambertMaterial({ color: 0xffffff, side: THREE.DoubleSide })
 
-      // add box 2 - tga texture
-      var texture2 = loader.load('textures/crate_color8.tga')
-      var material2 = new THREE.MeshPhongMaterial({ color: 0xffffff, map: texture2 })
+      var loader = new VTKLoader()
+      loader.load('models/vtk/bunny.vtk', function (geometry) {
+        geometry.center()
+        geometry.computeVertexNormals()
 
-      var mesh2 = new THREE.Mesh(geometry, material2)
-      mesh2.position.x = 50
+        var mesh = new THREE.Mesh(geometry, material)
+        mesh.position.set(-0.075, 0.005, 0)
+        mesh.scale.multiplyScalar(0.2)
+        scene.add(mesh)
+      })
 
-      scene.add(mesh2)
+      var loader1 = new VTKLoader()
+      loader1.load('models/vtk/cube_ascii.vtp', function (geometry) {
+        geometry.computeVertexNormals()
+        geometry.center()
 
-      //
+        var material = new THREE.MeshLambertMaterial({ color: 0x00ff00, side: THREE.DoubleSide })
+        var mesh = new THREE.Mesh(geometry, material)
 
-      var ambientLight = new THREE.AmbientLight(0xffffff, 0.4)
-      scene.add(ambientLight)
+        mesh.position.set(-0.025, 0, 0)
+        mesh.scale.multiplyScalar(0.01)
 
-      var light = new THREE.DirectionalLight(0xffffff, 1)
-      light.position.set(1, 1, 1)
-      scene.add(light)
+        scene.add(mesh)
+      })
 
-      //
+      var loader2 = new VTKLoader()
+      loader2.load('/static/models/vtk/cube_binary.vtp', function (geometry) {
+        geometry.computeVertexNormals()
+        geometry.center()
 
-      renderer = new THREE.WebGLRenderer({ antialias: true })
+        var material = new THREE.MeshLambertMaterial({ color: 0x0000ff, side: THREE.DoubleSide })
+        var mesh = new THREE.Mesh(geometry, material)
+
+        mesh.position.set(0.025, 0, 0)
+        mesh.scale.multiplyScalar(0.01)
+
+        scene.add(mesh)
+      })
+
+      var loader3 = new VTKLoader()
+      loader3.load('models/vtk/cube_no_compression.vtp', function (geometry) {
+        geometry.computeVertexNormals()
+        geometry.center()
+
+        var material = new THREE.MeshLambertMaterial({ color: 0xff0000, side: THREE.DoubleSide })
+        var mesh = new THREE.Mesh(geometry, material)
+
+        mesh.position.set(0.075, 0, 0)
+        mesh.scale.multiplyScalar(0.01)
+
+        scene.add(mesh)
+      })
+
+      // renderer
+
+      renderer = new THREE.WebGLRenderer()
       renderer.setPixelRatio(window.devicePixelRatio)
       renderer.setSize(window.innerWidth, window.innerHeight)
+
+      let container = document.getElementById('3d')
       container.appendChild(renderer.domElement)
 
-      //
+      // controls
 
-      var controls = new OrbitControls(camera, renderer.domElement)
+      controls = new TrackballControls(camera, renderer.domElement)
 
-      //
+      controls.rotateSpeed = 5.0
+      controls.zoomSpeed = 5
+      controls.panSpeed = 2
 
-      let stats = new Stats()
+      controls.staticMoving = true
+
+      stats = new Stats()
       container.appendChild(stats.dom)
 
       //
-
       window.addEventListener('resize', this.onWindowResize, false)
       this.camera = camera
       this.scene = scene
@@ -107,25 +147,23 @@ export default {
       this.material = material
       this.mesh = mesh
       this.renderer = renderer
-      this.controls = controls
-      this.stats = stats
     },
+
     onWindowResize () {
       let camera = this.camera
       let renderer = this.renderer
+      let controls = this.controls
       camera.aspect = window.innerWidth / window.innerHeight
       camera.updateProjectionMatrix()
       renderer.setSize(window.innerWidth, window.innerHeight)
+      controls.handleResize()
     },
+
     animate () {
       requestAnimationFrame(this.animate)
-      this.render()
+      this.controls.update()
+      this.renderer.render(this.scene, this.camera)
       this.stats.update()
-    },
-    render () {
-      let camera = this.camera
-      let scene = this.scene
-      this.renderer.render(scene, camera)
     }
     // ########## methods #########
   }
