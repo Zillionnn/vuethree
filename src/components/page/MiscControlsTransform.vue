dc99c474cc4001806f6026ca03a51b4889f8c754
Administrator <skyestzhang@163.com>
2020-01-22 14:24:28 +0800

    misc controls transform

--- src/components/page/MiscControlsTransform.vue
+++ src/components/page/MiscControlsTransform.vue
@@ -1,6 +1,6 @@
 <template>
  <div class="page">
-     <div id="box"></div>
+     <div id="3d"></div>
  </div>
 </template>
 
@@ -18,13 +18,7 @@ export default {
       camera: null,
       scene: null,
       renderer: null,
-
-      geometry: null,
-      material: null,
-      mesh: null,
-
       control: null,
-      stats: null,
       orbit: null
     }
   },
@@ -34,20 +28,10 @@ export default {
   },
   methods: {
     init () {
-      let camera = null
-      let scene = null
-      //   let geometry = null
-      // let material = null
-      //   let mesh = null
-      let renderer = null
-      let control = null
-      let stats = null
-      let orbit = null
-
       this.renderer = new THREE.WebGLRenderer()
       this.renderer.setPixelRatio(window.devicePixelRatio)
       this.renderer.setSize(900, 600)
-      document.getElementById('box').appendChild(this.renderer.domElement)
+      document.body.appendChild(this.renderer.domElement)
 
       //
 
@@ -62,114 +46,98 @@ export default {
       light.position.set(1, 1, 1)
       this.scene.add(light)
 
-      var texture = new THREE.TextureLoader().load('textures/crate.gif', this.render())
+      var texture = new THREE.TextureLoader().load('/static/textures/crate.gif', this.render)
       texture.anisotropy = this.renderer.capabilities.getMaxAnisotropy()
 
-      let geometry = new THREE.BoxBufferGeometry(200, 200, 200)
-      var material = new THREE.MeshLambertMaterial({ map: texture, transparent: true })
+      var geometry = new THREE.BoxBufferGeometry(200, 200, 200)
+      // var material = new THREE.MeshLambertMaterial({ map: texture, transparent: true })
+      let material = new THREE.MeshNormalMaterial()
 
       this.orbit = new OrbitControls(this.camera, this.renderer.domElement)
       this.orbit.update()
-      this.orbit.addEventListener('change', this.render())
+      this.orbit.addEventListener('change', this.render)
 
       this.control = new TransformControls(this.camera, this.renderer.domElement)
-      this.control.addEventListener('change', this.render())
-
+      this.control.addEventListener('change', this.render)
       this.control.addEventListener('dragging-changed', (event) => {
         this.orbit.enabled = !event.value
       })
 
-      let mesh = new THREE.Mesh(geometry, material)
+      var mesh = new THREE.Mesh(geometry, material)
       this.scene.add(mesh)
 
       this.control.attach(mesh)
       this.scene.add(this.control)
 
-      window.addEventListener('resize', this.onWindowResize(), false)
+      // window.addEventListener('resize', onWindowResize, false)
 
-    //   window.addEventListener('keydown', function (event) {
-    //     switch (event.keyCode) {
-    //       case 81: // Q
-    //         control.setSpace(control.space === 'local' ? 'world' : 'local')
-    //         break
+      // window.addEventListener('keydown', function (event) {
+      //   switch (event.keyCode) {
+      //     case 81: // Q
+      //       control.setSpace(control.space === 'local' ? 'world' : 'local')
+      //       break
 
-    //       case 16: // Shift
-    //         control.setTranslationSnap(100)
-    //         control.setRotationSnap(THREE.Math.degToRad(15))
-    //         control.setScaleSnap(0.25)
-    //         break
+      //     case 16: // Shift
+      //       control.setTranslationSnap(100)
+      //       control.setRotationSnap(THREE.Math.degToRad(15))
+      //       control.setScaleSnap(0.25)
+      //       break
 
-    //       case 87: // W
-    //         control.setMode('translate')
-    //         break
+      //     case 87: // W
+      //       control.setMode('translate')
+      //       break
 
-    //       case 69: // E
-    //         control.setMode('rotate')
-    //         break
+      //     case 69: // E
+      //       control.setMode('rotate')
+      //       break
 
-    //       case 82: // R
-    //         control.setMode('scale')
-    //         break
+      //     case 82: // R
+      //       control.setMode('scale')
+      //       break
 
-    //       case 187:
-    //       case 107: // +, =, num+
-    //         control.setSize(control.size + 0.1)
-    //         break
+      //     case 187:
+      //     case 107: // +, =, num+
+      //       control.setSize(control.size + 0.1)
+      //       break
 
-    //       case 189:
-    //       case 109: // -, _, num-
-    //         control.setSize(Math.max(control.size - 0.1, 0.1))
-    //         break
+      //     case 189:
+      //     case 109: // -, _, num-
+      //       control.setSize(Math.max(control.size - 0.1, 0.1))
+      //       break
 
-    //       case 88: // X
-    //         control.showX = !control.showX
-    //         break
+      //     case 88: // X
+      //       control.showX = !control.showX
+      //       break
 
-    //       case 89: // Y
-    //         control.showY = !control.showY
-    //         break
+      //     case 89: // Y
+      //       control.showY = !control.showY
+      //       break
 
-    //       case 90: // Z
-    //         control.showZ = !control.showZ
-    //         break
+      //     case 90: // Z
+      //       control.showZ = !control.showZ
+      //       break
 
-    //       case 32: // Spacebar
-    //         control.enabled = !control.enabled
-    //         break
-    //     }
+      //     case 32: // Spacebar
+      //       control.enabled = !control.enabled
+      //       break
+      //   }
+      // })
 
-    //     this.camera = camera
-    //     this.scene = scene
-    //     this.geometry = geometry
-    //     this.material = material
-    //     this.mesh = mesh
-    //     this.renderer = renderer
-    //     this.control = control
-    //     this.stats = stats
-    //   })
-
-    //   window.addEventListener('keyup', function (event) {
-    //     switch (event.keyCode) {
-    //       case 17: // Ctrl
-    //         control.setTranslationSnap(null)
-    //         control.setRotationSnap(null)
-    //         control.setScaleSnap(null)
-    //         break
-    //     }
-    //   })
+      // window.addEventListener('keyup', function (event) {
+      //   switch (event.keyCode) {
+      //     case 17: // Ctrl
+      //       control.setTranslationSnap(null)
+      //       control.setRotationSnap(null)
+      //       control.setScaleSnap(null)
+      //       break
+      //   }
+      // })
     },
-
-    onWindowResize () {
-      this.camera.aspect = window.innerWidth / window.innerHeight
-      this.camera.updateProjectionMatrix()
-      this.renderer.setSize(window.innerWidth, window.innerHeight)
-      this.render()
-    },
-
     render () {
+      console.log(this.orbit)
+
       this.renderer.render(this.scene, this.camera)
     }
-
   }
 }
 </script>
